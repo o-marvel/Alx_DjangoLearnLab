80b10141be7776c4271e043056fdd44844af2ae7
o-marvel <osemekemrvlls@gmail.com>
2026-01-18 14:50:54 +0000

    creating views with function and classes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,3 +8,19 @@ urlpatterns = [
     path("books/", list_books, name="book-list"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
 ]
+
+# relationship_app/urls.py
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    # Login view with custom template
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    
+    # Logout view with custom template
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    
+    # Register view
+    path('register/', views.register, name='register'),
+]
