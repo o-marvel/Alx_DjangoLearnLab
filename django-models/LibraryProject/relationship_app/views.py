708bc75cb7e42ec3e578c4e83d762628ba7f59ea
o-marvel <osemekemrvlls@gmail.com>
2026-01-18 21:54:52 +0000

    login, logout and regsiter done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,4 +37,4 @@ def register(request):
     else:
         form = UserCreationForm()
 
-    return render(request, "register.html", {"form": form})
+    return render(request, "relationship_app/register.html", {"form": form})
