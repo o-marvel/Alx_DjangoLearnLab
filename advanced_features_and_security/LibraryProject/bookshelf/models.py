ff3427aee73745783de11d6cd0b7fa24425f2505
o-marvel <osemekemrvlls@gmail.com>
2026-01-24 11:31:49 +0000

    added custom user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +8,13 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
