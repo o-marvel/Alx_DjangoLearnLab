66462bb861175b62398e8cba066e8db3afad9f7c
o-marvel <osemekemrvlls@gmail.com>
2026-01-24 12:28:52 +0000

    Create and Configure Groups with Assigned Permissions-2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_book(request):
+    return render(request, "bookshelf/create_book.html")
+
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request):
+    return render(request, "bookshelf/delete_book.html")
