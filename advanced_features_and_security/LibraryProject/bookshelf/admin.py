ef5ebace993a8f6e7a5248889a948049f81f4660
o-marvel <osemekemrvlls@gmail.com>
2026-01-24 11:58:17 +0000

    modifying the admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+# from main_django.advanced_features_and_security1.LibraryProject.relationship_app.admin import CustomUserAdmin
 from .models import Book
 
 
@@ -7,3 +9,18 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author")
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
