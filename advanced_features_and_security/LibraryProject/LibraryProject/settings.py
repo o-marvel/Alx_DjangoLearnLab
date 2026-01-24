99a34674d7d67f9c242ad832208f562be551fdf3
o-marvel <osemekemrvlls@gmail.com>
2026-01-24 02:57:24 +0000

    adv security added

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
-
-
 
 # Application definition
 
@@ -119,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'relationship_app.CustomUser'
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,33 +1,58 @@
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import CustomUser
+
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
+
+#     fieldsets = UserAdmin.fieldsets + (
+#         ("Additional Info", {
+#             "fields": ("date_of_birth", "profile_photo"),
+#         }),
+#     )
+
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         ("Additional Info", {
+#             "fields": ("date_of_birth", "profile_photo"),
+#         }),
+#     )
+    
+# # admin.site.register(CustomUser, CustomUserAdmin)
+# # Register your models here.
+
+
+
+
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = UserAdmin.fieldsets + (
+#         ("Additional Info", {
+#             "fields": ("date_of_birth", "profile_photo"),
+#         }),
+#     )
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+    """
+    Admin configuration for CustomUser.
+    Ensures new fields appear in Django admin.
+    """
 
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {
+        ("Additional Information", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
 
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Info", {
-            "fields": ("date_of_birth", "profile_photo"),
-        }),
-    )
-    
-admin.site.register(CustomUser, CustomUserAdmin)
-# Register your models here.
-
-
-
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {
+        ("Additional Information", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
 
+    list_display = ("username", "email", "is_staff", "is_active")
