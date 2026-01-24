2377c6cf6af60016faf928d6cf74d04573d31147
o-marvel <osemekemrvlls@gmail.com>
2026-01-24 12:25:30 +0000

    Create and Configure Groups with Assigned Permissions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -49,3 +49,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
