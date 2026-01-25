a80e0f6a5eb9790e7afd0f49c8c54f99232a489f
o-marvel <osemekemrvlls@gmail.com>
2026-01-25 00:44:52 +0000

    add forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,8 +3,6 @@ from django import forms
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=True)
 
-from django import forms
-
 class ExampleForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True)
